9852c156c6625d766bdbf49ceec1870b277c77cc
maulana4444 <84928555+maulana4444@users.noreply.github.com>
2021-06-05 16:31:22 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -41,11 +41,10 @@ bot.on('message', (msg) => {
                 r
             ]
         ).then((jres1)=>{
-            //console.log(jres1);
-            v = parseFloat(jres1[0])
-		p = parseFloat(jres1[1])
+            console.log(jres1);
+            
             //cls_model.classify([parseFloat(s[0]), parseFloat(s[1]), parseFloat(jres1[0]), parseFloat(jres1[1])]).then((jres2)=>{
-		cls_model.classify([i, r, v, p]).then((jres2)=>{
+		cls_model.classify([i, r, parseFloat(jres1[0]), parseFloat(jres1[1])]).then((jres2)=>{
                 bot.sendMessage(
                         msg.chat.id,
                         `nilai v yang diprediksi adalah ${jres1[0]} volt`
