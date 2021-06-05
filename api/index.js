08ab8a644d13e86bcb161933269464a650373ad2
maulana4444 <84928555+maulana4444@users.noreply.github.com>
2021-06-05 15:29:03 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -38,10 +38,9 @@ bot.on('message', (msg) => {
                 parseFloat(s[1])
             ]
         ).then((jres1)=>{
-            //console.log(jres1);
+            console.log(jres1);
             
             cls_model.classify([parseFloat(s[0]), parseFloat(s[1]), parseFloat(jres1[0]), parseFloat(jres1[1])]).then((jres2)=>{
-                
                 bot.sendMessage(
                         msg.chat.id,
                         `nilai v yang diprediksi adalah ${jres1[0]} volt`
@@ -55,7 +54,7 @@ bot.on('message', (msg) => {
                         `KLASIFIKASI TEGANGAN ${jres2}`
                 );
                 state = 0;
-            }
+            })
         })
     }else{
         bot.sendMessage(
